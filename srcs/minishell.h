87ddae72d0b4e80fc382ff2e9da73e3f4c3b389c
Rino Naito <rinodelevingne@gmail.com>
2023-06-28 20:00:50 +0900

    modify #01

--- srcs/minishell.h
+++ srcs/minishell.h
@@ -6,7 +6,7 @@
 /*   By: rnaito <rnaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/30 20:38:11 by rnaito            #+#    #+#             */
-/*   Updated: 2023/06/27 21:32:41 by rnaito           ###   ########.fr       */
+/*   Updated: 2023/06/28 17:50:51 by rnaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,14 +22,24 @@
 
 typedef enum e_token_type {
 	TK_WORD,
-	TK_RESERVED,
 	TK_OPERATOR,
 }		t_token_type;
 
 typedef struct s_token {
-	char				*word;
+	char				*token;
 	t_token_type		type;
 	struct s_token		*next;
 }						t_token;
+
+//tokenize.c
+t_token *ft_tokenize(char *line);
+char	*ft_get_token(char **line);
+
+//libft_plus.c
+t_token	*ft_lstnew_ms(char *token, int	type);
+t_token	*ft_lstlast_ms(t_token *node);
+void	ft_lstadd_back_ms(t_token **head, t_token *new);
+char	*ft_strchrchr(char *str, char c1, char c2);
+char	*ft_strndup(char *str, size_t len);
 
 #endif
