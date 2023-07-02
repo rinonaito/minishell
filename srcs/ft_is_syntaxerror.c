e7c0e070eb2236c8a3c5db5db0f441b08bbfc42e
Rino Naito <rinodelevingne@gmail.com>
2023-07-02 17:12:20 +0900

    ADD: ft_is_syntaxerror.c #1

--- srcs/ft_is_syntaxerror.c
+++ srcs/ft_is_syntaxerror.c
@@ -6,12 +6,17 @@
 /*   By: rnaito <rnaito@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/01 20:31:19 by rnaito            #+#    #+#             */
-/*   Updated: 2023/07/02 15:55:42 by rnaito           ###   ########.fr       */
+/*   Updated: 2023/07/02 17:11:46 by rnaito           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+//@func: check if there is syntax error 
+//@param:
+//	t_token *head: head of the list t_token
+//@return_val: return 1 if there is syntax error. 
+// return 0 if the syntax is correct.
 int	ft_is_syntaxerror(t_token *head)
 {
 	t_token	*token;
