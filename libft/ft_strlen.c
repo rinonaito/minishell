60f50e41ea6d96dd64c670cdb0e8df8fecf9f17e
Takumi Araki <taaraki@c1r10s6.42tokyo.jp>
2023-07-11 23:06:45 +0900

    MOD: modifying

--- libft/ft_strlen.c
+++ libft/ft_strlen.c
@@ -6,7 +6,7 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/07 20:52:10 by taaraki           #+#    #+#             */
-/*   Updated: 2023/02/15 12:01:27 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/07/11 22:59:03 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ size_t	ft_strlen(const char *s)
 {
 	size_t	len;
 
+	if (!s)
+		return (0);
 	len = 0;
 	while (s[len])
 		len++;
--- srcs/exe_create_process.c
+++ srcs/exe_create_process.c
@@ -6,7 +6,7 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 16:43:25 by taaraki           #+#    #+#             */
-/*   Updated: 2023/07/11 20:28:36 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/07/11 22:51:33 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,19 +23,38 @@ void	child_process(int fd[2], char **cmd_args, char **env, int num_cmds, int i);
 void	parent_process(int fd[2]);//, int i)
 */
 
-int	create_process(t_tree *root, char **env)
+//@func: create processes, including parent/child/wait processes
+//@return_val:
+//		 exit status of wait process		
+int	create_process(t_tree *root, char **cmd_args, char **env)
 {
 	int	fd[2];
 	int	i;
 	pid_t	pid;
 	//char	***cmd_args = {{"ls", "-l", NULL}, {"wc", "-l", NULL}, {"cat", NULL}};
-	char	*cmd_args[] = {"ls", "-l", NULL};
+	//char	*cmd_args[] = {"ls", "-l", NULL};
 	//
-	int	num_cmds = 3;
 	int	status;
 
 
-	printf("create_process: %s\n", root->param->token);
+	printf("%s\n", __func__);
+	/*** BEGIN ***/
+	/*
+	if (!cmd_args)
+	{
+		printf("!cmd_args\n");
+		return (1);
+	}
+	i = 0;
+	while (cmd_args[i])
+	{
+		printf(" i:%d,[%s]\n", i, cmd_args[i]);
+		i++;
+	}
+	return (1);
+	*/
+	/*** END ***/
+
 	i = 0;
 	//while (i < num_cmds)
 	//{
@@ -45,9 +64,15 @@ int	create_process(t_tree *root, char **env)
 		if (pid == -1)
 			ft_perror("fork\n");
 		else if (pid == 0)
+		{
+			printf("# child\n");
 			child_process(fd, cmd_args, env);//, num_cmds, i);
+		}
 		else
+		{
+			printf("# parent\n");
 			parent_process(fd);
+		}
 		//i++;
 	//}	
 	wait(&status);
