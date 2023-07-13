d862c0332ccc10ababa448754ed5ccdffd4cd0ee
Takumi Araki <taaraki@c1r8s8.42tokyo.jp>
2023-07-13 21:15:54 +0900

    MOD: modifying wait_process to run inside trace_tree_entry

--- srcs/exe_wait_process.c
+++ srcs/exe_wait_process.c
@@ -6,7 +6,7 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 16:43:31 by taaraki           #+#    #+#             */
-/*   Updated: 2023/07/10 17:21:34 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/07/13 20:58:38 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,20 +15,50 @@
 #include	<stdlib.h>
 #include	<unistd.h>
 
-int	wait_process(int pid, int num_cmds)
+int	wait_process(pid_t pid)//, int num_cmds)
 {
+	int		status;
+	pid_t	check_pid;
+
+
+	printf(">%s\n", __func__);
+	printf(" ### before while ###\n");
+	///*
+	while ((check_pid = waitpid(-1, &status, 0)) > 0)
+	{
+		//check_pid = waitpid(-1, &status, 0);
+		//wait(&status);
+		printf(" check_pid:[%d]\n", check_pid);
+		if (check_pid == pid)
+		{
+			printf(" check_pid:[%d] status:%d\n", check_pid, status);
+		    if (WIFEXITED(status))
+			{
+        		printf(" [%s] Exit: %d\n", __func__, WEXITSTATUS(status));
+        		printf(" [%s] Exit: %d\n", __func__, status);
+				return (WEXITSTATUS(status));
+			}
+		}
+	}
+        		printf(" [%s] Exit: %d\n", __func__, status);
+		    if (WIFEXITED(status))
+        		printf(" [%s] Exit: %d\n", __func__, WEXITSTATUS(status));
+	printf(" ### after while ###\n");
+	return (status);
+}
+/*
 	int	status;
 	int	i;
 
 	//printf("%s\n", __func__);
 	i = 0;
-///*
 	while (i < num_cmds - 1)
 	{
 		wait(&status);
 		i++;
 	}
-//*/
 	waitpid(pid, &status, 0);
 	return (status);
 }
+{
+*/
