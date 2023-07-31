46831b071f7e2baccd62445aee7ddc41a6066ac9
Takumi Araki <taaraki@c1r8s7.42tokyo.jp>
2023-07-31 23:39:24 +0900

    MOD: wrong user name??

--- srcs/builtin_exit.c
+++ srcs/builtin_exit.c
@@ -1,26 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   builtin_pwd.c                                      :+:      :+:    :+:   */
+/*   builtin_exit.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/07/31 22:23:05 by taaraki           #+#    #+#             */
-/*   Updated: 2023/07/31 23:38:15 by taaraki          ###   ########.fr       */
+/*   Created: 2023/07/31 23:37:47 by taaraki           #+#    #+#             */
+/*   Updated: 2023/07/31 23:37:48 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include	"minishell.h"
 
-int		builtin_pwd(t_cmds *cmds_info)
-{
-	char	buff_cwd[PATH_MAX];
-	int		ret;
-	
-	printf(" >%s\n", __func__);
-	ft_memset(buff_cwd, '\0', PATH_MAX);
-	if (!getcwd(buff_cwd, PATH_MAX))
-		return (-1);
-	printf("%s\n", buff_cwd);
-	return (0);
-}
