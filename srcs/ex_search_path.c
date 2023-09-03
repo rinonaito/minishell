17aeb4a4127fc104fee5c669a6077307cd476397
Takumi Araki <taaraki@c1r9s8.42tokyo.jp>
2023-09-03 22:38:49 +0900

    MOD: ex_search_path

--- srcs/ex_search_path.c
+++ srcs/ex_search_path.c
@@ -6,13 +6,13 @@
 /*   By: taaraki <taaraki@student.42.jp>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/10 16:33:54 by taaraki           #+#    #+#             */
-/*   Updated: 2023/09/03 17:23:14 by taaraki          ###   ########.fr       */
+/*   Updated: 2023/09/03 22:37:12 by taaraki          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include	"minishell.h"
 
-void	ft_strncpy(char *dst, char *src, int n)
+static void	ft_strncpy(char *dst, char *src, int n)
 {
 	int	i;
 
@@ -27,6 +27,45 @@ void	ft_strncpy(char *dst, char *src, int n)
 	dst[i] = '\0';
 }
 
+//if absolute path
+static char	*absolute_path(char *filename, char *value)
+{
+	filename_wo_command = ft_strndup(filename, length_of_commands);
+	while (value != NULL)
+	{
+		ft_bzero(path, PATH_MAX);
+		end = ft_strchr(value, ':');
+		if (end != NULL)
+			ft_strncpy(path, value, end - value);
+		else
+			break ;
+		//printf(" path1:[%s]\n", path);
+		/*** ***/
+		//if absolute path
+		//if (ft_strequ(filename_wo_command, path))
+		//{
+			//if (access(filename, F_OK | X_OK) == 0)
+				//return (ft_strdup(path));
+		//}
+		/*** ***/
+		ft_strlcat(path, "/", PATH_MAX);
+		ft_strlcat(path, filename, PATH_MAX);
+		//duplicated = NULL;
+		if (access(path, F_OK | X_OK) == 0)
+			return (ft_strdup(path));
+		value = end + 1;
+		//printf(" value:[%s]\n", value);
+		//printf(" path2:[%s]\n", path);
+	}
+	return (NULL);
+	if (ft_strequ(filename_wo_command, path))
+	{
+		if (access(filename, F_OK | X_OK) == 0)
+		return (ft_strdup(path));
+	}
+	return (NULL);
+}
+
 //@func: get the path name in the form of "/path/cmd"
 //@args:
 //		 const char *filename: command name
@@ -37,10 +76,16 @@ char	*ft_search_path(const char *filename, t_env *env_lst)
 {
 	char	path[PATH_MAX];
 	char	*value;
-	char	*duplicated;
+	//char	*duplicated;
 	char	*end;
+	//
+	char	*filename_wo_command;
 
+	printf(" >>%s\n", __func__);
+	printf(" filename:[%s]\n", filename);
 	value = my_getenv("PATH", env_lst);
+	if (filename[0] == '/')
+		return (absolute_path(filename, value));
 	while (value != NULL)
 	{
 		ft_bzero(path, PATH_MAX);
@@ -49,15 +94,26 @@ char	*ft_search_path(const char *filename, t_env *env_lst)
 			ft_strncpy(path, value, end - value);
 		else
 			break ;
+		//printf(" path1:[%s]\n", path);
+		/*** ***/
+		//if absolute path
+		//if (ft_strequ(filename_wo_command, path))
+		//{
+			//if (access(filename, F_OK | X_OK) == 0)
+				//return (ft_strdup(path));
+		//}
+		/*** ***/
 		ft_strlcat(path, "/", PATH_MAX);
 		ft_strlcat(path, filename, PATH_MAX);
-		duplicated = NULL;
+		//duplicated = NULL;
 		if (access(path, F_OK | X_OK) == 0)
-		{
-			duplicated = ft_strdup(path);
-			return (duplicated);
-		}
+			return (ft_strdup(path));
 		value = end + 1;
 	}
 	return (NULL);
 }
+		//if (access(path, F_OK | X_OK) == 0)
+		//{
+			//duplicated = ft_strdup(path);
+			//return (duplicated);
+		//}
